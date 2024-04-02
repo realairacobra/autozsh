8e3e13b59087fa5c84e622b0720a604a4568bfc1
airacobra <93622023+realairacobra@users.noreply.github.com>
2024-04-02 16:17:36 -0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt upgrade
 
 # Install zsh
-sudo apt install -y zsh nano wget flatpak
+sudo apt install -y zsh nano wget flatpak git
 
 # Install Oh My Zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
